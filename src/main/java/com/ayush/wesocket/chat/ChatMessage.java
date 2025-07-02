d3121672197a30508c65cf83a8a04023249e9ead
Ayush Agrawal <130290786+AyushAgrawal31@users.noreply.github.com>
2025-07-02 17:48:57 +0530

    Add files via upload

---
+++ src/main/java/com/ayush/wesocket/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.ayush.websocket.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
