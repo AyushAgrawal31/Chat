342b00db294a10a883357014f6d1c7e3cac62e99
Ayush Agrawal <130290786+AyushAgrawal31@users.noreply.github.com>
2025-07-02 18:17:19 +0530

    Add files via upload

--- src/test/java/com/ayush/websockets/ChatApplicationTests.java
+++ src/test/java/com/ayush/websockets/ChatApplicationTests.java
@@ -1,1 +1,13 @@
+package com.ayush.websocket;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
